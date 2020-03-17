c315db52d13a8143c000ebd376114332a9c5602b
Rahat Hossain <rahathossain690@gmail.com>
2020-03-18 02:30:16 +0600

    files added

---
+++ CSVReader.py
@@ -0,0 +1,21 @@
+import csv
+from tabulate import tabulate 
+
+def CSVReader(filename):
+	with open(filename) as csv_file:
+		csv_reader = csv.reader(csv_file, delimiter=',')
+		
+		print("Contest_id\tHandle\tSolve\tUpsolve");
+
+		for row in csv_reader:
+			string = "{0}\t{1}\t{2}\t{3}".format(row[0], row[1], row[2], row[3])
+			'''
+			row[0] = contestId
+			row[1] = handle
+			row[2] = Solve on contest time
+			row[3] = upsolve
+			'''
+			print(string)
+
+if __name__ == '__main__':
+    CSVReader("Codeforces.csv")
---
+++ parser.py
@@ -0,0 +1,100 @@
+'''
+CF Parser 
+Created By Rahat Hossain
+Data : 18 March, 2020
+
+Cautions:
+CF does not allow api call more than five times in a second.
+So you may use time.sleep(0.2) everytime you make a GET Request for safety :3
+'''
+
+import requests, json, time, csv
+
+def parse():
+	# gets input and calls further actions
+	
+	contestIds = []
+	handles = []
+	
+	# takes contestIds from file : contestId.txt
+	with open("contestId.txt") as contestFile:
+		contestIds = contestFile.read().split()
+
+	# takes handles from file : handles.txt
+	with open("handles.txt") as handleFile:
+		handles = handleFile.read().split()
+
+	# stores final data
+	finalList = []
+
+	# loops over all contests and handles
+	for contestId in contestIds:
+		for handle in handles:
+			data = process(contestId, handle)
+			finalList.append([contestId, handle, data[0], data[1]])
+	
+	writeCSV(finalList)
+	print("Success: file located at Codeforces.csv")
+
+def process(contestId, handle):
+	# processes data for specific contestId and handle
+	
+	url = "https://codeforces.com/api/contest.status"
+	params = {"contestId" : contestId, "handle" : handle} 
+
+	# api call
+	r = requests.get(url = url, params = params) 
+
+	# sudhu data r data...
+	wholeData = r.json()
+	
+	# check its status
+	if(wholeData['status'] != 'OK'):
+		raise Exception('Data not recieved for' + ' ' + handle + ' on contest : ' + contestId)
+		return (0, 0)
+
+	# retrieving actual data
+	data = wholeData["result"]
+
+	# holds data in respect of problems
+	solutionDict = {}
+
+	for submission in reversed(data):
+		problemId = submission["problem"]["index"]
+		participantType = submission["author"]["participantType"]
+		verdict = submission["verdict"]
+	
+		if problemId not in solutionDict:
+			if verdict == "OK" :
+				if participantType == "CONTESTANT":
+					solutionDict[problemId] = 1
+				else :
+					solutionDict[problemId] = 0.5
+			else :
+				solutionDict[problemId] = 0
+		else :
+			if solutionDict[problemId] == 0:
+				if verdict == "OK" :
+					if participantType == "CONTESTANT":
+						solutionDict[problemId] = 1
+					else :
+						solutionDict[problemId] = 0.5
+
+	downsolve, upsolve = 0, 0
+
+	for key in solutionDict:
+		if solutionDict[key] == 1:
+			downsolve += 1
+		elif solutionDict[key] == 0.5:
+			upsolve += 1
+
+	time.sleep(0.2)
+	return (downsolve, upsolve)
+
+def writeCSV(finalList):
+	with open("Codeforces.csv", 'w') as file:
+		writer = csv.writer(file)
+		writer.writerows(finalList)
+
+if __name__ == '__main__':
+    parse()
